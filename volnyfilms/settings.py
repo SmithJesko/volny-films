fa122dfdc1809eb7e22e495c293f6ca467172253
Smith Jesko <smithjesko@gmail.com>
2021-02-21 20:38:36 -0600

    added django-heroku

--- volnyfilms/settings.py
+++ volnyfilms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '^f&x068aya3z#nqn&*wi%%k%-2quo(zc_+acu^d6zqs(^9%u_('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://volny-films.herokuapp.com/', 'volnyfilms.com', '127.0.0.1']
 
 
 # Application definition
@@ -127,3 +128,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+django_heroku.settings(locals())
