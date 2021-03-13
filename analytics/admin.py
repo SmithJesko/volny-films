02e31908964acb60eae83549dc03c5be66a9e18c
Smith Jesko <smithjesko@gmail.com>
2021-03-13 11:04:01 -0600

    admin | changed display format

--- analytics/admin.py
+++ analytics/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import ClientConnection, UserClientConnection, MovieView
 
-admin.site.register(ClientConnection)
-admin.site.register(UserClientConnection)
+class UserClientConnectionAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'url', 'user', 'region_code', 'country_code', 'timestamp')
+
+class ClientConnectionAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'url', 'region_code', 'country_code', 'timestamp')
+
+admin.site.register(UserClientConnection, UserClientConnectionAdmin)
+admin.site.register(ClientConnection, ClientConnectionAdmin)
 admin.site.register(MovieView)
